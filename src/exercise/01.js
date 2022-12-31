eb0ec4a09931c541be36f067ac4961984dd5f229
GregoryBa <gregory.ba.95@gmail.com>
2022-12-31 10:16:40 +0100

    01 extra

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,20 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName}) {
+  const [name, setName] = React.useState(initialName)
+  // create a state
+  // '' = initial value of that state
   function handleChange(event) {
     setName(event.target.value)
+    // when you call set setName - trigger rerender of that component
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -21,7 +24,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Gregory" />
 }
 
 export default App
